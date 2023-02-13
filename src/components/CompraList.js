42a92116b9cb78666c0cdf2fbeae2099a4c369df
Abel Cumbane <abelrosac@gmail.com>
2023-02-13 12:30:11 +0000

    feat(removaCompra): adicao do metodo remover

--- src/components/CompraList.js
+++ src/components/CompraList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import CompraForm from './CompraForm'
+import Compra from './Compra'
 
 function CompraList() {
     const [compras, setCompras] = useState([]);
@@ -12,10 +13,26 @@ function CompraList() {
 
         setCompras(newCompras);
     };
+
+    const removeCompra = id => {
+        const removeArr = [...compras].filter(compra => compra.id !== id);
+        setCompras(removeArr);
+    }
+
+    const completeCompra = id => {
+        let updatedCompras = compras && compras.map(compra => {
+            if (compra.id === id) {
+                compra.isComplete = !compra.isComplete;
+            }
+            return compra;
+        });
+        setCompras(updatedCompras);
+    }
     return (
         <div>
             <h1>Lista de compras: </h1>
             <CompraForm onSubmit={addCompra} />
+            <Compra compras={compras} completeCompra={completeCompra} removeCompra={removeCompra} />
         </div>
     )
 }
