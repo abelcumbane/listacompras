5508635c9fd33ecb4798b7c98b4f998984fc1b48
Abel Cumbane <abelrosac@gmail.com>
2023-02-13 16:11:40 +0000

    chore: criacao da funcao submitUpdte

--- src/components/Compra.js
+++ src/components/Compra.js
@@ -3,14 +3,28 @@ import CompraForm from './CompraForm'
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 
-function Compra({ compras, completeCompra, removeCompra }) {
+function Compra({ compras, completeCompra, removeCompra, updateCompra }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     });
+
+    const submitUpdate = value => {
+        updateCompra(edit.id, value);
+        setEdit({
+            id: null,
+            value: ''
+        });
+    };
+
+
+    if (edit.id) {
+        return <CompraForm edit={edit} onSubmit={submitUpdate} />;
+    }
+
     return compras.map((compra, index) => (
         <div
-            className={compra.isComplete ? 'compra-row complete' : 'todo-row'}
+            className={compra.isComplete ? 'compra-row complete' : 'compra-row'}
             key={index}
         >
             <div key={compra.id} onClick={() => completeCompra(compra.id)}>
